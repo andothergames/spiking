68d4b0c59cefd0d0bf1dc34bdc2a6af936c90ba5
Joy <32204909+andothergames@users.noreply.github.com>
2024-07-30 13:04:57 +0100

    oauth spiking

---
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "Hello there";
+    }
+    @GetMapping("/secured")
+    public String secured(@AuthenticationPrincipal OAuth2User user){
+        return user.toString();
+    }
+}
